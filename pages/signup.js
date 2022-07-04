935823a5d44834909f80e7706d3f0e2c3c324c18
Kaloyan Dimitrov <kaloyankd.06@gmail.com>
2022-07-04 18:11:02 +0300

    Fixed sign up loading bug.

--- pages/signup.js
+++ pages/signup.js
@@ -6,15 +6,18 @@ export default function SignUp() {
 	const [spinner, setSpinner] = useState(true);
 	return (
 		<PageWrapper text="Sign up for">
-			{spinner && (
-				<span className={styles.spinner}>
-					<span className={styles.loader}></span>
-				</span>
-			)}
-			<iframe
-				src="https://docs.google.com/forms/d/e/1FAIpQLSdsaYDRkoBzpWnSYTwyC1DFaOCJZNSzZxgTEtw6ig-rlVxZNA/viewform?usp=sf_link"
-				onLoad={() => setSpinner(false)}
-			/>
+			<div className={styles.container}>
+				{spinner && (
+					<span className={styles.spinner}>
+						<span className={styles.loader}></span>
+					</span>
+				)}
+				<iframe
+					src="https://docs.google.com/forms/d/e/1FAIpQLSdsaYDRkoBzpWnSYTwyC1DFaOCJZNSzZxgTEtw6ig-rlVxZNA/viewform?usp=sf_link"
+					className={styles.frame}
+					onLoad={() => setSpinner(false)}
+				/>
+			</div>
 		</PageWrapper>
 	);
 }
