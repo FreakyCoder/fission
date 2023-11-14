9760b4020894e92677d64df1d1db7cc5e98ae701
Kaloyan Dimitrov <kaloyankd.06@gmail.com>
2023-11-14 23:32:01 +0200

    Changed navigation UI.

--- components/PageWrapper.js
+++ components/PageWrapper.js
@@ -1,22 +1,12 @@
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import Logo from "./Logo";
 import styles from "../styles/PageWrapper.module.css";
 
-export default function PageWrapper({ text, children }) {
+export default function PageWrapper({ children }) {
 	return (
 		<div className={styles.container}>
 			<NavBar />
 			<main className={styles.main}>
-				<section className={styles.side}>
-					<h1>{text}</h1>
-					<span className={styles.fission}>
-						<span className={styles.logo}>
-							<Logo />
-						</span>
-						<b>FISSION</b>
-					</span>
-				</section>
 				<section className={styles.content}>
 					{children}
 					<Footer />
